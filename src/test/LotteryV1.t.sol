caa55e5f409c58b63c75663c5028879e88033b98
oraclown <owen.burton13@gmail.com>
2022-04-14 20:46:58 -0400

    Test choose/pay winner

--- src/test/LotteryV1.t.sol
+++ src/test/LotteryV1.t.sol
@@ -10,10 +10,10 @@ import "forge-std/console.sol";
 contract ContractTest is DSTest, stdCheats {
     LotteryV1 private lottery;
     Vm vm = Vm(HEVM_ADDRESS);
-    // MockERC20 token;
 
     address alice = address(0x12341234);
     address bob = address(0x67896789);
+    address cletus = address(0x12345678);
 
     uint256 public MAX_TICKETS = 100;
     uint256 public TICKET_PRICE = 1;
@@ -21,6 +21,8 @@ contract ContractTest is DSTest, stdCheats {
     uint256 public ADMIN_FEE = 1; // 1%
 
     function setUp() public {
+        vm.deal(cletus, 1 ether);
+        vm.prank(cletus);
         lottery = new LotteryV1(
             MAX_TICKETS,
             TICKET_PRICE,
@@ -34,7 +36,7 @@ contract ContractTest is DSTest, stdCheats {
         vm.label(address(this), "TestContract");
     }
 
-    function testDeployContract() public {
+    function test_deployContract() public {
         assertTrue(!lottery.payWinnerCalled());
         assertEq(address(lottery).balance, 0);
         assertEq(lottery.maxTickets(), MAX_TICKETS);
@@ -43,10 +45,7 @@ contract ContractTest is DSTest, stdCheats {
         assertEq(lottery.lotteryEnd(), block.timestamp + BUY_PERIOD);
         assertEq(lottery.forTheBoyz(), ADMIN_FEE);
         assertEq(lottery.ticketsBought(), 0);
-        assertEq(
-            lottery.admin(),
-            address(0xb4c79daB8f259C7Aee6E5b2Aa729821864227e84)
-        );
+        assertEq(lottery.admin(), cletus);
         assertEq(lottery.winner(), address(0));
     }
 
@@ -100,6 +99,44 @@ contract ContractTest is DSTest, stdCheats {
         lottery.buyTicket{value: TICKET_PRICE}();
 
         vm.stopPrank();
+    }
+
+    function testChooseAndPayWinner() public {
+        vm.roll(10); // Avoid arithmetic underflow when calculating block.number - 1
+
+        vm.deal(alice, 1 ether);
+        vm.deal(bob, 1 ether);
+
+        uint256 aliceStartBalance = alice.balance;
+        uint256 bobStartBalance = bob.balance;
+        uint256 cletusStartBalance = cletus.balance;
+
+        vm.startPrank(alice);
+        for (uint256 i = 0; i < MAX_TICKETS; i++) {
+            lottery.buyTicket{value: TICKET_PRICE}();
+        }
+        vm.stopPrank();
+        uint256 pot = address(lottery).balance;
+        vm.warp(BUY_PERIOD + 1); // fast forward
+
+        vm.startPrank(bob);
+        lottery.chooseWinner();
+        lottery.payWinner();
+        vm.stopPrank();
+
+        uint256 aliceReward = alice.balance - aliceStartBalance;
+        uint256 bobReward = bob.balance - bobStartBalance;
+
+        assertEq(lottery.winner(), alice);
+        assertTrue(lottery.payWinnerCalled());
+        assertEq(bob.balance, bobStartBalance + TICKET_PRICE * 2);
+        assertEq(address(lottery).balance, 0);
+        console.log("Cletus balance", cletus.balance);
+        console.log("Pot", pot);
+        console.log("Alic balance", alice.balance);
+        console.log("Bob balance", bob.balance);
+        uint256 adminPayout = pot - aliceReward - bobReward;
+        assertEq(cletus.balance, cletusStartBalance + adminPayout);
     }
 
     function testChooseWinnerFail() public {
